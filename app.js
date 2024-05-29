1ab763792aa40f8167ac4c5ce1f15e6a93335358
Agustin Romero Rivero <agusrr1305@gmail.com>
2024-05-29 17:28:25 +0200

    first

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req,res) =>{
+    res.status(200).json({
+    message: "hola mundo",
+        
+    })
+})
+
+app.listen(port,() =>{
+console.log(`servidor escuchando en http://localhost:${port}`)
+})
